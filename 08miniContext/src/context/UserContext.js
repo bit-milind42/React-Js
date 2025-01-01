6c5299411657d5e3759407b7232fef429f859456
MILIND SINGH <milluk813@gmail.com>
2025-01-01 17:01:46 +0530

    React js

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+// user context is a provider (user context bnne k baad ek provider bnaya jata hai)
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+
